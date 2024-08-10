596228860a88406b5103ef952f11cc8b7c77e09d
malhardv <malhar.vhatkar@gmail.com>
2024-08-10 16:00:28 +0530

    first-commit

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PROJECTS } from '../constants'
+
+const Projects = () => {
+  return (
+    <div className='border-b border-neutral-900 pb-4'></div>
+  )
+}
+
+export default Projects;
